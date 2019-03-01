a913a65c3d80e485b4836baee47d30fe92dbfdb9
KH Chan <kwunhok@gmail.com>
2019-03-01 14:30:03 +0800

    Fix error log issue

--- pkg/aws/util.go
+++ pkg/aws/util.go
@@ -10,14 +10,14 @@ import (
 func GetLocalRegion() string {
 	resp, err := http.Get("http://169.254.169.254/latest/meta-data/placement/availability-zone/")
 	if err != nil {
-		glog.Errorf("unable to get current region information", err)
+		glog.Errorf("unable to get current region information, %v", err)
 		return ""
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		glog.Errorf("cannot read response from instance metadata", err)
+		glog.Errorf("cannot read response from instance metadata, %v", err)
 	}
 
 	// strip the last character from AZ to get region ID
